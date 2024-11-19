26e480e1d143ccfcd635f20020e88cf296995bfb
David van Oudheusden <doudheusden@novacollege.nl>
2024-11-19 10:59:55 +0100

    feature: added form

---
+++ www/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            margin: 0px;
+
+        }
+
+        form {
+            margin: 20px;
+        }
+
+        label{
+            width: 100px;
+            display: inline-block;
+        }
+
+        .form-group{
+            margin:20px 0px;
+        }
+
+    </style>
+</head>
+
+<body>
+
+    <form action="" method="post">
+        <div class="form-group">
+            <label for="voornaam">Voornaam</label>
+            <input type="text" name="" id="voornaam">
+        </div>
+        <div class="form-group">
+            <label for="tussenvoegsel">Tussenvoegsel</label>
+            <input type="text" name="" id="tussenvoegsel">
+        </div>
+
+        <div class="form-group">
+            <label for="achternaam">Achternaam</label>
+            <input type="text" name="" id="achternaam">
+        </div>
+        <div class="form-group">
+            <label for="straat">Straat</label>
+            <input type="text" name="" id="straat">
+        </div>
+        <div class="form-group">
+            <label for="huisnummer">Huisnummer</label>
+            <input type="text" name="" id="huisnummer">
+        </div>
+        <div class="form-group">
+            <label for="postcode">Postcode</label>
+            <input type="text" name="" id="postcode">
+        </div>
+        <div class="form-group">
+            <label for="plaats">Plaats</label>
+            <input type="text" name="" id="plaats">
+        </div>
+        <div class="form-group">
+            <label for="land">Land</label>
+            <input type="text" name="" id="land">
+        </div>
+
+
+
+    </form>
+
+
+
+
+</body>
+
+</html>
