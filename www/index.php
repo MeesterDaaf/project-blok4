1e0608a075246486148c85e3a09670dbaa446d06
David van Oudheusden <doudheusden@novacollege.nl>
2024-11-25 09:35:13 +0100

    feature: formulier uitgebreid

--- www/index.php
+++ www/index.php
@@ -5,68 +5,99 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        body {
-            margin: 0px;
-
-        }
-
-        form {
-            margin: 20px;
-        }
-
-        label{
-            width: 100px;
-            display: inline-block;
-        }
-
-        .form-group{
-            margin:20px 0px;
-        }
-
-    </style>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
     <form action="process_form.php" method="post">
-        <div class="form-group">
-            <label for="voornaam">Voornaam</label>
-            <input type="text" name="voornaam" id="voornaam" />
-        </div>
-        <div class="form-group">
-            <label for="tussenvoegsel">Tussenvoegsel</label>
-            <input type="text" name="tussenvoegsel" id="tussenvoegsel">
-        </div>
+        <div class="container">
+            <div class="column">
+                <div class="form-group">
+                    <label for="voornaam">Voornaam</label>
+                    <input type="text" name="voornaam" id="voornaam" placeholder="Uw voornaam aub">
+                </div>
+                <div class="form-group">
+                    <label for="tussenvoegsel">Tussenvoegsel</label>
+                    <input type="text" name="tussenvoegsel" id="tussenvoegsel">
+                </div>
 
-        <div class="form-group">
-            <label for="achternaam">Achternaam</label>
-            <input type="text" name="achternaam" id="achternaam">
-        </div>
-        <div class="form-group">
-            <label for="straat">Straat</label>
-            <input type="text" name="straat" id="straat">
-        </div>
-        <div class="form-group">
-            <label for="huisnummer">Huisnummer</label>
-            <input type="text" name="huisnummer" id="huisnummer">
-        </div>
-        <div class="form-group">
-            <label for="postcode">Postcode</label>
-            <input type="text" name="postcode" id="postcode">
-        </div>
-        <div class="form-group">
-            <label for="plaats">Plaats</label>
-            <input type="text" name="plaats" id="plaats">
-        </div>
-        <div class="form-group">
-            <label for="land">Land</label>
-            <input type="text" name="land" id="land">
-        </div>
+                <div class="form-group">
+                    <label for="achternaam">Achternaam</label>
+                    <input type="text" name="achternaam" id="achternaam">
+                </div>
 
-        <button type="submit">Sla op</button>
-        
+                <div class="form-group flex">
+                    
+                    <label for="red">Rood</label>
+                    <input type="radio" name="kleur" id="red" value="red">
+                    
+                    <label for="green">Groen</label>
+                    <input type="radio" name="kleur" id="green" value="green">
 
+                    <label for="yellow">Geel</label>
+                    <input type="radio" name="kleur" id="yellow" value="yellow">
+
+                    <label for="blue">Blauw</label>
+                    <input type="radio" name="kleur" id="blue" value="blue">
+                </div>
+            </div>
+            <div class="column">
+                <div class="form-group">
+                    <label for="straat">Straat</label>
+                    <input type="text" name="straat" id="straat" placeholder="Hier uw straat aub">
+                </div>
+                <div class="form-group">
+                    <label for="huisnummer">Huisnummer</label>
+                    <input type="text" name="huisnummer" id="huisnummer">
+                </div>
+                <div class="form-group">
+                    <label for="postcode">Postcode</label>
+                    <input type="text" name="postcode" id="postcode">
+                </div>
+                <div class="form-group">
+                    <label for="plaats">Plaats</label>
+                    <input type="text" name="plaats" id="plaats">
+                </div>
+                <div class="form-group">
+                    <label for="land">Land</label>
+                    <input type="text" name="land" id="land">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="thema">Kies thema</label>
+                <select name="thema" id="thema">
+                    <option value="zakelijk">Zakelijk</option>
+                    <option value="speels">Speels</option>
+                    <option value="kleurrijk">Kleurrijk</option>
+                </select>
+                <div>
+                    <label for="">Nieuwsbrief ontvangen?</label>
+                    <input type="radio" name="nieuwsbrief" id="" value="ja">
+                    <input type="radio" name="nieuwsbrief" id="" value="nee">
+                </div>
+                <div>
+                    <label for="voetbal">Voetbal</label>
+                    <input type="checkbox" name="hobby" id="voetbal">
+                    <label for="schaken">Schaken</label>
+                    <input type="checkbox" name="schaken" id="schaken">
+                </div>
+            </div>
+          
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+            <button type="submit">Sla op</button>
     </form>
 
 
--- www/process_form.php
+++ www/process_form.php
@@ -1,5 +1,22 @@
 <?php
 
+if($_SERVER["REQUEST_METHOD"] !== "POST"){
+    echo "Je hebt de verkeerde request method gebruikt";
+    exit;
+}
+
+if( !   isset($_POST["voornaam"])     ){
+    echo "De sleutel voornaam bestaat niet. foutje";
+    exit;
+}
+
+
+if(    empty($_POST["voornaam"])   ){
+    echo "Het veld voornaam is verplicht!!";
+    exit;
+}
+
+
 $voornaam = $_POST["voornaam"];
 $tussenvoegsel = $_POST["tussenvoegsel"];
 $achternaam = $_POST["achternaam"];
@@ -9,11 +26,18 @@ $postcode = $_POST["postcode"];
 $plaats = $_POST["plaats"];
 $land = $_POST["land"];
 
+$kleur = $_POST["kleur"];
+$thema = $_POST["thema"];
+
+
 require 'database.php';
 
 
-$sql = "INSERT INTO users (firstname, infix, lastname, street, housenumber, zipcode, city, country ) 
-            VALUES ('$voornaam', '$tussenvoegsel', '$achternaam', '$straat', '$huisnummer', '$postcode', '$plaats', '$land')";
+//sla ook de kleur en het thema op in de database
+
+
+$sql = "INSERT INTO users (firstname, infix, lastname, street, housenumber, zipcode, city, country, color, theme ) 
+            VALUES ('$voornaam', '$tussenvoegsel', '$achternaam', '$straat', '$huisnummer', '$postcode', '$plaats', '$land', '$kleur', '$thema')";
 
 mysqli_query($conn, $sql);
 
